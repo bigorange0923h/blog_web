af7fad49d72e26b677f96b9a8789b59935da25b5
BigOrange <bigorange.weicheng@gmail.com>
2021-05-30 21:01:12 +0800

    头部head 组件

---
+++ src/components/HomeHead.vue
@@ -0,0 +1,135 @@
+<!-- 博客首页头部 -->
+<template>
+  <div class="home-banner">
+    <div class="banner-container">
+      <transition name="fade">
+        <h1 class="blog-title animated zoomIn">欢迎</h1>
+      </transition>
+      <h2>来到大橙子的博客</h2>
+      <!-- 向下滚动 -->
+      <div class="scroll-down" @click="scrollDown">
+        <h4><i class="el-icon-arrow-down"></i></h4>
+      </div>
+    </div>
+    <!--中间内容,如果太窄了可放到container    <div  class="m-container m-padded-tb-big">-->
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HomeHead",
+  data() {
+    return {};
+  },
+  methods: {},
+  components: {},
+  created() {},
+};
+</script>
+
+<style scoped lang="scss">
+.home-banner {
+  position: absolute;
+  top: 0px;
+  left: 0;
+  right: 0;
+  height: 100vh;
+  background: url("http://r.photo.store.qq.com/psc?/V53KcXfb1umonn4HbITu3rINxs43TczD/45NBuzDIW489QBoVep5mcQaBtLM2yTpYe999VZqnRjqLW3e23.UCR78O5Km8SpsknNgOGpEzdY7QHY1usDO6pbksfeQBV5CqlMGgsjJVV9s!/r")
+    center center / cover no-repeat;
+  background-color: #49b1f5;
+  background-attachment: fixed;
+  text-align: center;
+  color: #fff !important;
+  animation: header-effect 1s !important;
+}
+.banner-container {
+  margin-top: 43vh;
+  line-height: 1.5;
+  color: #eee;
+}
+@media (min-width: 760px) {
+  .blog-title {
+    font-size: 2.5rem;
+  }
+  .blog-intro {
+    font-size: 1.5rem;
+  }
+  .blog-contact {
+    line-height: 40px;
+    text-align: center;
+    font-size: 1.5rem;
+    margin: 6px 0 -6px;
+  }
+  .home-container {
+    max-width: 1200px;
+    margin: calc(100vh - 50px) auto 0 auto;
+    padding: 0 3px;
+  }
+  .article-card {
+    display: flex;
+    align-items: center;
+    height: 280px;
+    width: 100%;
+    margin-top: 20px;
+  }
+  .article-cover {
+    overflow: hidden;
+    height: 100%;
+    width: 45%;
+  }
+  .on-hover {
+    transition: all 0.6s;
+  }
+  .article-card:hover .on-hover {
+    transform: scale(1.1);
+  }
+  .article-wrapper {
+    padding: 0 2.5rem;
+    width: 55%;
+  }
+  .article-wrapper a {
+    font-size: 1.5rem;
+    transition: all 0.3s;
+  }
+}
+@media (max-width: 759px) {
+  .blog-title {
+    font-size: 24px;
+  }
+  .blog-contact {
+    font-size: 1.25rem;
+    line-height: 2;
+  }
+  .home-container {
+    width: 100%;
+    margin: calc(100vh - 66px) auto 0 auto;
+  }
+  .article-card {
+    margin-top: 1rem;
+  }
+  .article-cover {
+    border-radius: 8px 8px 0 0 !important;
+    height: 230px !important;
+    width: 100%;
+  }
+  .article-cover div {
+    border-radius: 8px 8px 0 0 !important;
+  }
+  .article-wrapper {
+    padding: 1.25rem 1.25rem 1.875rem;
+  }
+  .article-wrapper a {
+    font-size: 1.25rem;
+    transition: all 0.3s;
+  }
+}
+.scroll-down {
+  cursor: pointer;
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+}
+.scroll-down i {
+  font-size: 2rem;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,40 +1,15 @@
 <template>
-  <div>
-    <!-- <div class="page_header_div"></div> -->
-    <el-container>
-      <el-header style="background: black">
-        <!-- 导航菜单 -->
-        <el-menu
-          :default-active="activeIndex"
-          class="el-menu-demo"
-          mode="horizontal"
-          @select="handleSelect"
-          active-text-color="white"
-          router="true"
-        >
-          <el-menu-item index="1" route="/home">主页</el-menu-item>
-          <el-menu-item index="2">用户管理</el-menu-item>
-          <el-menu-item index="3">消息中心</el-menu-item>
-          <el-menu-item index="4">订单管理</el-menu-item>
-        </el-menu>
-      </el-header>
-
-      <el-main style="height: 800px">
-        <router-view />
-      </el-main>
-      <el-footer>Footer</el-footer>
-    </el-container>
-  </div>
+  <HomeHead>11111</HomeHead>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import HomeHead from "@/components/HomeHead.vue";
 
 export default {
   name: "Home",
   components: {
-    HelloWorld,
+    HomeHead,
   },
   data() {
     return {};
@@ -42,13 +17,4 @@ export default {
 };
 </script>
 <style scoped lang="scss">
-.page_header_div {
-  background: url(../assets/home/bg.jpg);
-  -webkit-background-size: cover;
-  -moz-background-size: cover;
-  -o-background-size: cover;
-  background-size: cover;
-  width: 100%;
-  height: 500px;
-}
 </style>
