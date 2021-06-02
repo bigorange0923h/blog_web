610f4218e05e26105a35fce7390727f186c6c850
huangweicheng <574445631@qq.com>
2021-06-02 18:16:31 +0800

    新增屏幕大小样式

--- src/components/HomeHead.vue
+++ src/components/HomeHead.vue
@@ -2,12 +2,11 @@
 <template>
   <div class="home-banner">
     <el-affix :offset="0">
-      <div id="top-navbar">
-        <div id="top-navbar-left">
+      <div class="top-navbar mobile-hidden">
+        <div class="top-navbar-left">
           <p>Big Orange</p>
         </div>
-
-        <div id="top-navbar-right">
+        <div class="top-navbar-right">
           <el-menu
             :default-active="activeIndex"
             class="el-menu"
@@ -29,6 +28,19 @@
           </el-menu>
         </div>
       </div>
+      <div class="mobile-navbar pc-hidden">
+        <el-button
+          class="mobile-navbar-btn"
+          @click="drawer = true"
+          type="primary"
+          style="margin-left: 16px"
+        >
+          <i class="el-icon-menu"></i>
+        </el-button>
+      </div>
+      <el-drawer class="menu_drawer" v-model="drawer" :with-header="false">
+        <div class="top-navbar-context"></div>
+      </el-drawer>
     </el-affix>
 
     <div class="banner-container">
@@ -49,7 +61,10 @@ import TopNavBar from "@/components/TopNavBar.vue";
 export default {
   name: "HomeHead",
   data() {
-    return {};
+    return {
+      drawer: false,
+      scrollTopTemp: 0,
+    };
   },
   methods: {
     // 初始化
@@ -65,7 +80,9 @@ export default {
         window.pageYOffset ||
         document.documentElement.scrollTop ||
         document.body.scrollTop;
-      console.log(scrollTop);
+      var scroll = scrollTop - this.scrollTopTemp;
+      if (scroll > 0) {
+      }
     },
   },
   components: {},
@@ -83,7 +100,7 @@ export default {
   left: 0;
   right: 0;
   height: 100vh;
-  background: url("../assets/home/bg.png") center center / cover no-repeat;
+  background: url("../assets/home/tree.jpg") center center / cover no-repeat;
   // background: black;
   background-attachment: fixed;
   text-align: center;
@@ -158,31 +175,98 @@ export default {
   font-size: 2rem;
 }
 
-//================================================================  顶部水平导航栏 ================================================================
-#top-navbar {
+//================================================================  顶部水平导航栏 start ================================================================
+.top-navbar {
   width: 100%;
   animation: header-effect 1s !important;
   height: 60px;
-  background-color: #fff;
+  background-color: rgb(44, 43, 43);
+  color: white;
   border-bottom: 2px solid transparent;
 }
-#top-navbar-left {
+.top-navbar-left {
   height: 100%;
   float: left;
 }
-#top-navbar-left p {
+.top-navbar-left p {
   margin-top: 10px;
   margin-left: 20px;
   font-size: 22px;
 }
-#top-navbar-right {
+.top-navbar-right {
   height: 100%;
   float: right;
 }
 
-// ==================================================================================================================================================
+.mobile-navbar-btn {
+  float: right;
+  background-color: rgb(0, 0, 0, 0);
+  border: none;
+}
+// elementui 组件
+.el-menu {
+}
+// 隐藏elementui导航栏整体下划线
+.el-menu.el-menu--horizontal {
+  border-bottom: none;
+}
+.el-menu--horizontal > .el-menu-item {
+  float: left;
+  height: 60px;
+  line-height: 60px;
+  margin: 0;
+  color: #909399;
+  background-color: rgb(44, 44, 44);
+}
+.el-menu--horizontal > .el-menu-item.is-active {
+  color: #ffffff;
+  border-bottom: 2px solid #ffffff;
+}
 
+.el-menu--horizontal > .el-menu-item:not(.is-disabled):hover,
+.el-menu--horizontal > .el-menu-item:not(.is-disabled):focus {
+  i {
+    color: #fdfdfd;
+  }
+  color: #fdfdfd;
+  background-color: rgb(44, 44, 44);
+  font: 0.5em;
+}
+
+//================================================================  顶部水平导航栏 end ================================================================
+
+//================================================================  垂直导航栏 start ================================================================
+.mobile-navbar-btn {
+  background-color: rgba(255, 251, 251, 0);
+  color: #ffffff;
+  font: 0.5em;
+}
+
+.el-drawer__body {
+  background-color: black !important;
+  color: green;
+}
+
+//.el-drawer.ltr, .el-drawer.rtl
+.el-drawer {
+  background-color: rgb(44, 44, 44, 0) !important;
+}
+
+//修改按钮获取焦点时的样式
+.el-button--primary:hover,
+.el-button--primary:focus {
+  background-color: rgb(44, 44, 44, 0);
+}
+// .top-navbar-context {
+//   width: 100%;
+//   height: 100%;
+//   background-color: black !important;
+// }
+//================================================================  垂直导航栏 end ================================================================
 @media (min-width: 760px) {
+  .pc-hidden {
+    display: none;
+  }
   .blog-title {
     font-size: 3rem;
   }
@@ -256,6 +340,9 @@ export default {
   .article-wrapper a {
     font-size: 1.25rem;
     transition: all 0.3s;
+  }
+  .mobile-hidden {
+    display: none;
   }
 }
 </style>
