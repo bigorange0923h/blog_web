4a32bf44bc74d9226227c19e0717195f4c19d09c
huangweicheng <574445631@qq.com>
2021-06-09 16:47:50 +0800

    提交

--- src/components/BlogHead.vue
+++ src/components/BlogHead.vue
@@ -1,6 +1,6 @@
-<!-- 博客首页头部 -->
+<!-- 博客头部 -->
 <template>
-  <div class="home-banner">
+  <div>
     <el-affix :offset="0">
       <div class="top-navbar mobile-hidden">
         <div class="top-navbar-left">
@@ -80,15 +80,6 @@
     <div class="backtop-box pc-hidden">
       <el-backtop></el-backtop>
     </div>
-
-    <div class="banner-container">
-      <h1 class="blog-title animated zoomIn">欢迎</h1>
-      <h2>来到大橙子的博客</h2>
-      <!-- 向下滚动 -->
-      <div class="scroll-down" @click="scrollDown">
-        <i id="scroll-down" class="el-icon-arrow-down"></i>
-      </div>
-    </div>
   </div>
 </template>
 
@@ -97,7 +88,7 @@ import "../assets/css/animates.css";
 import TopNavBar from "@/components/TopNavBar.vue";
 
 export default {
-  name: "HomeHead",
+  name: "BlogHead",
   data() {
     return {
       drawer: false,
@@ -107,13 +98,6 @@ export default {
   },
   methods: {
     // 初始化
-    scrollDown() {
-      window.scrollTo({
-        behavior: "smooth",
-        top: document.documentElement.clientHeight,
-      });
-    },
-
     handleScroll() {
       var scrollTop =
         window.pageYOffset ||
@@ -137,16 +121,6 @@ export default {
 </script>
 
 <style scoped lang="scss">
-.home-banner {
-  position: absolute;
-  top: 0px;
-  left: 0;
-  right: 0;
-  height: 100vh;
-  background: url("../assets/home/tree.jpg") center center / cover no-repeat;
-  background-attachment: fixed;
-  text-align: center;
-}
 @keyframes header-effect {
   0% {
     opacity: 0.5;
@@ -158,63 +132,6 @@ export default {
     filter: none;
     transform: translateY(0);
   }
-}
-
-.banner-container {
-  margin-top: 43vh;
-  line-height: 1.5;
-  color: #eee;
-}
-#scroll-down {
-  width: 20px;
-  height: 20px;
-  position: relative;
-  animation: mymove 2s infinite;
-  -webkit-animation: mymove 2s infinite; /*Safari and Chrome*/
-}
-@keyframes mymove {
-  0% {
-    bottom: 0px;
-  }
-  25% {
-    bottom: 5px;
-  }
-  50% {
-    bottom: 10px;
-  }
-  75% {
-    bottom: 5px;
-  }
-  100% {
-    bottom: 0px;
-  }
-}
-
-@-webkit-keyframes mymove /*Safari and Chrome*/ {
-  0% {
-    bottom: 0px;
-  }
-  25% {
-    bottom: 5px;
-  }
-  50% {
-    bottom: 10px;
-  }
-  75% {
-    bottom: 5px;
-  }
-  100% {
-    bottom: 0px;
-  }
-}
-.scroll-down {
-  cursor: pointer;
-  position: absolute;
-  bottom: 0;
-  width: 100%;
-}
-.scroll-down i {
-  font-size: 2rem;
 }
 
 //================================================================  顶部水平导航栏 start ================================================================
@@ -374,38 +291,5 @@ export default {
     transition: all 0.3s;
   }
 }
-@media (max-width: 759px) {
-  .blog-title {
-    font-size: 24px;
-  }
-  .blog-contact {
-    font-size: 1.25rem;
-    line-height: 2;
-  }
-  .home-container {
-    width: 100%;
-    margin: calc(100vh - 66px) auto 0 auto;
-  }
-  .article-card {
-    margin-top: 1rem;
-  }
-  .article-cover {
-    border-radius: 8px 8px 0 0 !important;
-    height: 230px !important;
-    width: 100%;
-  }
-  .article-cover div {
-    border-radius: 8px 8px 0 0 !important;
-  }
-  .article-wrapper {
-    padding: 1.25rem 1.25rem 1.875rem;
-  }
-  .article-wrapper a {
-    font-size: 1.25rem;
-    transition: all 0.3s;
-  }
-  .mobile-hidden {
-    display: none;
-  }
-}
+
 </style>
