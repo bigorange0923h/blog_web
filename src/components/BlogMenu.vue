607018882abe75fed293e7dbd28f8cabdb7a847c
huangweicheng <574445631@qq.com>
2021-06-16 10:07:53 +0800

    增加了背景彩带

--- src/components/BlogMenu.vue
+++ src/components/BlogMenu.vue
@@ -1,45 +1,47 @@
-<!-- 博客头部菜单按钮 -->
+<!-- 博客头部菜
+单按钮 -->
 <template>
-  <el-affix :offset="15">
+  <el-affix :offset="10">
     <el-button
       :style="style"
       v-show="!drawer"
       class="menu-btn"
-      size="small"
+      size="mini"
       @click="drawer = true"
       plain
     >
       <i class="el-icon-menu"></i>
     </el-button>
-    <div class="menu-drawer">
-      <el-drawer title="菜单" v-model="drawer" :with-header="false" size="10">
-        <div class="ui fluid inverted vertical menu m-menu">
-          <h3 class="ui header center aligned item">菜单</h3>
-          <a href="#" class="m-item item m-mobile-hide"
-            ><i class="mini home icon"></i>首页</a
-          >
-          <a href="#" class="m-item item m-mobile-hide"
-            ><i class="mini idea icon"></i> 分类</a
-          >
-          <a href="#" class="m-item item m-mobile-hide"
-            ><i class="mini tags icon"></i>标签</a
-          >
-          <a href="#" class="m-item item m-mobile-hide"
-            ><i class="mini clone icon"></i>归档</a
-          >
-          <a href="#" class="m-item item m-mobile-hide"
-            ><i class="mini info icon"></i>关于我</a
-          >
-          <div class="right m-item item m-mobile-hide">
-            <div class="ui icon inverted transparent input">
-              <input type="text" placeholder="搜索..." />
-              <i class="search icon"></i>
-            </div>
+  </el-affix>
+  <div class="menu-drawer">
+    <el-drawer title="菜单" v-model="drawer" :with-header="false" size="10">
+      <div class="ui fluid inverted vertical menu m-menu">
+        <h3 class="ui header center aligned item">菜单</h3>
+        <a href="#" class="m-item item m-mobile-hide"
+          ><i class="mini home icon"></i>首页</a
+        >
+        <a href="#" class="m-item item m-mobile-hide"
+          ><i class="mini idea icon"></i> 分类</a
+        >
+        <a href="#" class="m-item item m-mobile-hide"
+          ><i class="mini tags icon"></i>标签</a
+        >
+        <a href="#" class="m-item item m-mobile-hide"
+          ><i class="mini clone icon"></i>归档</a
+        >
+        <a href="#" class="m-item item m-mobile-hide"
+          ><i class="mini info icon"></i>关于我</a
+        >
+        <div class="right m-item item m-mobile-hide">
+          <div class="ui icon inverted transparent input">
+            <input type="text" placeholder="搜索..." />
+
+            <i class="search icon"></i>
           </div>
         </div>
-      </el-drawer>
-    </div>
-  </el-affix>
+      </div>
+    </el-drawer>
+  </div>
 </template>
 
 <script>
@@ -54,14 +56,20 @@ export default {
   mounted() {
     window.addEventListener("scroll", this.windowScroll); //监听页面滚动
   },
+
   methods: {
     windowScroll() {
       let clientH = $(window).height(); //视窗大小
       let menubtnTop = $(".menu-btn").offset().top; //按钮距离顶部距离
       if (menubtnTop - clientH >= 10) {
-        this.style = { color: `#000000`, border: `1px solid #000000` };
+        this.style = {
+          border: `1px solid #000000`,
+          backgroundColor: `#000000`,
+          marginRight: `0px`,
+        };
       } else {
         this.style = { color: `#ffffff`, border: `1px solid #ffffff` };
+        this.size = "medium";
       }
       // 执行到变色的部分磁吸页面到顶部
       if (menubtnTop - clientH <= 5 && menubtnTop - clientH >= 3) {
