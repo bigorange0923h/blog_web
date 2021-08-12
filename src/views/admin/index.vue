16e780e1e45b4592b90f4616a581165375d86342
huangweicheng <574445631@qq.com>
2021-08-12 10:41:30 +0800

    后台首页头样式

--- src/views/admin/index.vue
+++ src/views/admin/index.vue
@@ -2,7 +2,42 @@
 <template>
   <!-- 首页头 -->
   <el-container>
-    <el-header name="header">Header</el-header>
+    <el-header name="header">
+      <el-row style="height: 100%">
+        <el-col :span="6">
+          <div><h1 class="m-margin-top-small">BigOrange</h1></div></el-col
+        >
+        <!-- 
+         <el-avatar
+              class="m-margin-top-small"
+              style="float: right"
+              src="https://cube.elemecdn.com/0/88/03b0d39583f48206768a7534e55bcpng.png"
+            ></el-avatar> -->
+        <el-col :span="6" :offset="12">
+          <el-dropdown
+            trigger="click"
+            class="m-margin-top-small"
+            style="float: right"
+          >
+            <span class="el-dropdown-link">
+              <el-avatar
+                src="https://cube.elemecdn.com/0/88/03b0d39583f48206768a7534e55bcpng.png"
+              ></el-avatar>
+            </span>
+            <template #dropdown>
+              <el-dropdown-menu>
+                <el-dropdown-item class="clearfix">
+                  评论
+                  <el-badge class="mark" :value="12" />
+                </el-dropdown-item>
+                <el-dropdown-item class="clearfix"> 设置 </el-dropdown-item>
+                <el-dropdown-item class="clearfix"> 注销 </el-dropdown-item>
+              </el-dropdown-menu>
+            </template>
+          </el-dropdown>
+        </el-col>
+      </el-row>
+    </el-header>
     <el-container>
       <el-aside width="200px">Aside</el-aside>
       <el-main>Main</el-main>
@@ -31,8 +66,8 @@ export default {
 
 <style  scoped>
 .el-header {
-  border-bottom: brown 1px solid !important;
-  color: #333;
+  border-bottom: 1px solid #dcdfe6 !important;
+  color: #409eff;
   height: 80px;
 }
 
